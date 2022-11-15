478b7a7b83e8d1910db76000ece33c6c821e3c26
linini1 <2369201548@qq.com>
2022-11-15 20:51:45 +0800

    Site updated: 2022-11-15 20:51:44

--- js/music.js
+++ js/music.js
@@ -1,5 +1,5 @@
 <link rel="stylesheet" class="aplayer-secondary-style-marker" href="\assets\css\APlayer.min.css"><script src="\assets\js\APlayer.min.js" class="aplayer-secondary-script-marker"></script><script class="meting-secondary-script-marker" src="\assets\js\Meting.min.js"></script>const ap = new APlayer({
-    container: document.getElementById('player'),	
+    container: document.getElementById('aplayer'),	
     mini: false,		
     autoplay: false,	
     theme: '#FADFA3',	
